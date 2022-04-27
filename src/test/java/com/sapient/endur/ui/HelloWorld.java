12ec92223b928d43d030689f31de6456acbd232d
Nayana N <nayn1@publicisgroupe.net>
2022-04-27 18:17:59 +0530

    project

---
+++ src/test/java/com/sapient/endur/ui/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.sapient.endur.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorld {
+
+	@Test
+	public void test() {
+		fail("assert(true)");
+	}
+
+}
